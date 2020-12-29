b311ee0a95853d7e64626ab0bc5b1582bd3b71c7
Pushkar Sharma <pushkarsharma8696@yahoo.com>
2020-12-29 16:49:51 +0530

    create emoji component to write emoji

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import ReactTypingEffect from 'react-typing-effect'
 import displaypic from './../assets/images/displayphotoxx.jpg'
 import Social from '../components/Social'
+import Emoji from '../components/Emoji'
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="condiv home">
+				<div>
+					<p style={{ fontSize: 30 }}>
+						Hey <Emoji symbol="👋" label="hello!" />
+					</p>
+				</div>
 				<img src={displaypic} className="displaypic" alt="profile_pic"></img>
 				<ReactTypingEffect
 					className="typingeffect"
